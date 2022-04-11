a99d5bc64aec5033cec6881e40e3dab0312fc49a
Parvu Eduard <parvueduard@gmail.com>
2022-04-12 00:15:26 +0300

    -

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -2,7 +2,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Catalog.API.Entities;
 
-public class Product
+public struct Product
 {
     [BsonId]
     [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
