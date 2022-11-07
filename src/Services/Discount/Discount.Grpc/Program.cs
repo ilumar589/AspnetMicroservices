cd7e1a8befec2cca0ea5ffe4673c5591526f7a9b
Parvu Eduard <parvueduard@gmail.com>
2022-11-07 22:42:10 +0200

    -

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,8 +1,8 @@
-using Discount.Grpc.Services;
 using Npgsql;
 using System.Data;
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Configuration;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,11 +18,12 @@ builder.Services.AddTransient<IDbConnection>(_ =>
     return new NpgsqlConnection(connectionUrl);
 });
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repository, IMapper mapper, ILogger<DiscountService> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override async Task<CuponModel> CreateDisocount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var cupon = _mapper.Map<Cupon>(request.Cupon);
+
+            await _repository.CreateDiscount(cupon);
+
+            _logger.LogInformation("Discount is successfully created. ProductName : {ProductName}", cupon.ProductName);
+
+            return request.Cupon;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDisocount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var deleted = await _repository.DeleteDiscount(request.ProductName);
+            return new DeleteDiscountResponse { Success = deleted };
+        }
+
+        public override async Task<CuponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var cupon = await _repository.GetDiscount(request.ProductName);
+            if (cupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not present"));
+            }
+
+            _logger.LogInformation("Discount is retrieved for ProductName: {ProductName}, Amount: {Amount}", cupon.ProductName, cupon.Amount);
+
+            var cuponModel = _mapper.Map<CuponModel>(cupon);
+            return cuponModel;
+        }
+
+        public override async Task<CuponModel> UpdateDisocount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var cupon = _mapper.Map<Cupon>(request.Cupon);
+            await _repository.UpdateDiscount(cupon);
+            _logger.LogInformation("Discount is successfully updated. ProductName : {ProductName}", cupon.ProductName);
+
+            return request.Cupon;
+        }
+    }
+}
